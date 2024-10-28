14f70ebbdd7cb503edac4115c64af175a4acdf73
yoona <103414935+yoonali@users.noreply.github.com>
2024-10-28 21:52:45 +0900

    feat: 입력값을 받아 자동차 객체 생성

---
+++ src/main/java/racingcar/Racingcar.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Racingcar {
+    public static final String INPUT_TEXT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String carName;
+
+    Racingcar(String carName) {
+        this.carName = carName;
+    }
+
+    public static void init() {
+        System.out.println(INPUT_TEXT);
+        List<Racingcar> cars = createCars();
+    }
+
+    public static List<Racingcar> createCars() {
+        String inputText = readLine();
+        String[] carNames = inputText.split(",");
+        List<Racingcar> cars = new ArrayList<>();
+
+        for (String name : carNames) {
+            if (name.length() <= 5) {
+                cars.add(new Racingcar(name.trim()));
+            } else {
+                throw new IllegalArgumentException("자동차 이름은 5글자 이하여야 합니다.");
+            }
+        }
+        return cars;
+    }
+
+}
